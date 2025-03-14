aee06a1bc24372b0ae3f509cb7149c04781c356e
Tiago Modesto <tiago.moura@ugpi.edu.br>
2025-03-13 22:43:58 -0300

    App.py

---
+++ Python/Restaurante-Final/Modelos/Restaurante_final.py
@@ -0,0 +1,26 @@
+class Restaurante:
+    restaurantes = []
+    def __init__(self, nome, categoria):
+        # init ----> mostrar quais instâncias vou usar
+        # self ---> referência da instância que estou usando
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
+        
+    def __str__(self):
+        
+        return f' {self._nome} | {self._categoria}'
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'.ljust(25)}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo.ljust(25)}')
+
+    @property
+    def ativo(self):
+        return '✅'if self._ativo else '❌'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+        
