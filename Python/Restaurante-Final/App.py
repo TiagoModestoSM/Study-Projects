fbe2eb5f4f5c088666612757d9a31cb0fbabc61d
Tiago Modesto <tiago.moura@ugpi.edu.br>
2025-03-13 23:34:20 -0300

    a

--- Python/Restaurante-Final/App.py
+++ Python/Restaurante-Final/App.py
@@ -1,11 +1,13 @@
-from Modelos.Restaurante_final import Restaurante
+from modelos.restaurante_final import Restaurante
 
 restaurante_praca = Restaurante('Praça','Gourmet')
-restaurante_sushi = Restaurante('MaoTseTung', 'Japonesa')
-restaurante_china = Restaurante('XingLing', 'Chinesa')
+restaurante_praca.receber_avaliacao('Tiago',10)
+restaurante_praca.receber_avaliacao('Ester', 8)
+restaurante_praca.receber_avaliacao('Sara', 6)
+
 
 def main():
     Restaurante.listar_restaurantes()
-
+    
 if __name__ == '__main__':
     main()
--- Python/Restaurante-Final/Modelos/Restaurante_final.py
+++ Python/Restaurante-Final/Modelos/Restaurante_final.py
@@ -1,3 +1,5 @@
+from modelos.avaliacao import Avaliacao
+
 class Restaurante:
     restaurantes = []
     def __init__(self, nome, categoria):
@@ -6,6 +8,7 @@ class Restaurante:
         self._nome = nome.title()
         self._categoria = categoria.upper()
         self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
         
     def __str__(self):
@@ -13,9 +16,9 @@ class Restaurante:
         return f' {self._nome} | {self._categoria}'
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'.ljust(25)}')
+        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} {'Status'.ljust(25)}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo.ljust(25)}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.media_notas} | {restaurante.ativo.ljust(25)}')
 
     @property
     def ativo(self):
@@ -23,4 +26,18 @@ class Restaurante:
     
     def alternar_estado(self):
         self._ativo = not self._ativo
+        
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
+        
+    @property
+    def media_notas(self):
+        if not self._avaliacao:
+            return 0
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return media
+        
         
