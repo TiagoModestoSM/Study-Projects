1b9b5506a506b1456257f105566dd9bdf89081bd
Tiago Modesto <tiago.moura@ugpi.edu.br>
2025-03-16 13:31:34 -0300

    a

--- Python/Restaurante-Final/App.py
+++ Python/Restaurante-Final/App.py
@@ -3,7 +3,7 @@ from modelos.restaurante_final import Restaurante
 restaurante_praca = Restaurante('Praça','Gourmet')
 restaurante_praca.receber_avaliacao('Tiago',10)
 restaurante_praca.receber_avaliacao('Ester', 8)
-restaurante_praca.receber_avaliacao('Sara', 6)
+restaurante_praca.receber_avaliacao('Sara', 1)
 
 
 def main():
--- Python/Teste.py
+++ Python/Teste.py
@@ -1,43 +1,19 @@
-nome_usuario = ''  # Variáveis globais para armazenar os dados do usuário
-senha_usuario = ''
-
-def informacao():
-    global nome_usuario, senha_usuario  # Referência às variáveis globais
-    print('Bem-vindo! Digite suas informações de login abaixo:')
-    opcao = int(input('Deseja se Cadastrar (1) ou fazer Login (2)?\n'))
+from datetime import datetime
+class Livro:
+    livro = []
+    def __init__(self,titulo, autor, ano_publicacao, disponivel = True):
+        ano_atual = datetime.now().year
+        if isinstance(ano_publicacao, int) and 1000 <= ano_publicacao <= ano_atual:
+            self._titulo  = titulo.title()
+            self._autor = autor.title()
+            self._ano_publicacao = ano_publicacao
+            self._disponivel = disponivel
+        else:
+            raise ValueError(f"O ano de publicação deve ser um número inteiro de 4 dígitos e não pode ser maior que {ano_atual}.")
+    @property
+    def disponivel(self):
+        return '✅'if self._disponivel else '❌'
     
-    if opcao == 1:
-        nome_usuario, senha_usuario = cadastro()  # Captura o retorno de cadastro
-        login(nome_usuario, senha_usuario)  # Realiza o login após o cadastro
-    elif opcao == 2:
-        if nome_usuario == '' and senha_usuario == '':  # Verifica se o cadastro foi feito
-            print('Faça seu cadastro primeiro.')
-            informacao()  # Chama novamente para o usuário se cadastrar
-        else:
-            login(nome_usuario, senha_usuario)  # Realiza o login com as credenciais cadastradas
-    else:
-        print('Digite um número válido.')
+livro_Tiago = Livro('Tiago','Tiago', 2000)
 
-def cadastro():
-    nome_usuario = input('Cadastre um nome para seu usuário: ')
-    senha_usuario = input('Cadastre uma senha para seu usuário: ')
-    return nome_usuario, senha_usuario  # Retorna as credenciais para a função informacao
-
-def login(nome_usuario, senha_usuario):
-    tentativas = 0
-    while True:
-        nome = input('Digite o nome de usuário: ')
-        senha = input('Digite sua senha: ')
-        tentativas += 1
-
-        if nome == nome_usuario and senha == senha_usuario:
-            print(f'Login bem-sucedido! Total de tentativas: {tentativas}')
-            break
-        else:
-            print(f'Seu nome de usuário ou senha estão incorretos. Tente novamente!\nTentativas feitas: {tentativas}')
-
-def main():
-    informacao()  # Inicia o processo de cadastro ou login
-
-if __name__ == '__main__':
-    main()  # Inicia o programa
+print(f'Nome do Livro: {livro_Tiago._titulo}, Autor: {livro_Tiago._autor}, Ano de Publicação: {livro_Tiago._ano_publicacao}, Disponível: {livro_Tiago.disponivel}')
