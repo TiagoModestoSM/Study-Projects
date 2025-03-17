4233d4dec77083b44e118121e51c4fa9f5df18fc
Tiago Modesto <tiago.moura@ugpi.edu.br>
2025-03-17 06:27:28 -0300

    test

--- Python/Restaurante-Final/App.py
+++ Python/Restaurante-Final/App.py
@@ -1,9 +1,9 @@
 from modelos.restaurante_final import Restaurante
 
 restaurante_praca = Restaurante('Praça','Gourmet')
-restaurante_praca.receber_avaliacao('Tiago',10)
-restaurante_praca.receber_avaliacao('Ester', 8)
-restaurante_praca.receber_avaliacao('Sara', 1)
+#restaurante_praca.receber_avaliacao('Tiago',10)
+#restaurante_praca.receber_avaliacao('Ester', 8)
+#restaurante_praca.receber_avaliacao('Sara', 1)
 
 
 def main():
