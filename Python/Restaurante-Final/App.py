ab202de47a0414b3cc3c955657195e45078dbb23
TiagoModestoSM <tiago.moura@ufpi.edu.br>
2025-04-03 09:46:35 -0300

    abc

--- Python/Restaurante-Final/App.py
+++ Python/Restaurante-Final/App.py
@@ -1,14 +1,18 @@
 from modelos.restaurante_final import Restaurante
 from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
+
+# Adicionando restaurante e bebidas
 restaurante_praca = Restaurante('Praça','Gourmet')
 bebida_refri = Bebida('Coca-Cola', 8, 'grande')
-prato_churras = Prato('Picanha', 20, 'FAZ O L')
+prato_churras = Prato('Picanha', 20, 'Churrasquiiin')
+
+# Adicionando no cardápio
 restaurante_praca.adicionar_no_cardapio(bebida_refri)
 restaurante_praca.adicionar_no_cardapio(prato_churras)
 def main():
-    print(bebida_refri)
-    print(prato_churras)
+
+    restaurante_praca.exibir_cardapio
      
 if __name__ == '__main__':
     main()
