fad7f82aacffd92d76f35ab75bf0c93462caa0cd
TiagoModestoSM <tiago.moura@ufpi.edu.br>
2025-04-08 10:19:52 -0300

    Nice

--- Python/Restaurante-Final/App.py
+++ Python/Restaurante-Final/App.py
@@ -7,6 +7,13 @@ from assests.exibir_subtitulo import exibir_subtitulo
 from assests.exibir_nome_do_programa import exibir_nome_do_programa
 
 
+def exibir_opcoes():
+    print('1. Cadastrar Restaurante')
+    print('2. Listar Restaurante')
+    print('3. Alterar Estado do Restaurante')
+    print('4. Remover Restaurante')
+    print('5. Sair')
+
 def escolher_opcoes():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
@@ -19,12 +26,13 @@ def escolher_opcoes():
         elif opcao_escolhida == 2:
             os.system('cls')
             exibir_subtitulo('LISTA DE RESTAURANTES')
-            Restaurante.listar_restaurantes()
+            listar_restaurante()
             voltar_menu()
         elif opcao_escolhida == 3:
             os.system('cls')
-            Restaurante.listar_restaurantes()
+            listar_restaurante()
             Restaurante.alternar_estado()
+            voltar_menu
         elif opcao_escolhida == 5:
             finalizar_app()
         else:
@@ -46,11 +54,21 @@ def voltar_menu():
 def finalizar_app():
    exibir_subtitulo('Finalizando App')
    exit
-    
+   
+
+def listar_restaurante():
+    print(f"{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'.ljust(25)} | {'Cadastro'.ljust(25)}")
+    for restaurante in Restaurante.restaurantes:
+        print(f"{restaurante._nome.ljust(25)} | "
+              f"{restaurante._categoria.ljust(25)} | "
+              f"{str(restaurante.media_notas).ljust(25)} | "
+              f"{str(restaurante.ativo).ljust(24)} | "
+              f"{str(restaurante._codigo_unico).ljust(25)}")
+
 def main():
     os.system('cls')
     exibir_nome_do_programa()
-    Restaurante.exibir_opcoes()
+    exibir_opcoes()
     escolher_opcoes()
 
 if __name__ == '__main__':
--- Python/Restaurante-Final/Modelos/Restaurante_final.py
+++ Python/Restaurante-Final/Modelos/Restaurante_final.py
@@ -6,9 +6,8 @@ import random
 class Restaurante:
     restaurantes = []
     numeros_cadastrados = []
+
     def __init__(self, nome, categoria):
-        # init ----> mostrar quais instâncias vou usar
-        # self ---> referência da instância que estou usando
         self._nome = nome.title()
         self._categoria = categoria.upper()
         self._ativo = False
@@ -16,31 +15,22 @@ class Restaurante:
         self._cardapio = []
         self._codigo_unico = None
         Restaurante.restaurantes.append(self)
-        
+
     def __str__(self):
-        
-        return f' {self._nome} | {self._categoria}'
-    
-    def exibir_opcoes():
-        print('1. Cadastrar Restaurante')
-        print('2. Listar Restaurante')
-        print('3. Alterar Estado do Restaurante')
-        print('4. Remover Restaurante')
-        print('5. Sair')
-    
-    
+        return f'{self._nome} | {self._categoria}'
+
+
     @classmethod
     def cadastrar_novo_restaurante(cls):
         nome = input("Digite o nome do restaurante: ").strip()
         categoria = input("Digite a categoria: ").strip()
 
         if nome and categoria:
-            novo_restaurante = cls(nome, categoria)  # Criando uma instância da classe
-            novo_restaurante._codigo_unico = cls.codigo_unico()  # Agora sim: atribuindo o código depois de criar o objeto
+            novo_restaurante = cls(nome, categoria)
+            novo_restaurante._codigo_unico = cls.codigo_unico()
             print(f'Restaurante "{novo_restaurante._nome}" cadastrado com sucesso em {data_hora.data_formatada} às {data_hora.hora_formatada} ')
         else:
             print("Nome ou categoria inválidos.")
-
 
     @classmethod
     def codigo_unico(cls):
@@ -51,23 +41,27 @@ class Restaurante:
                 return codigo
 
     @classmethod
-    def listar_restaurantes(cls):
-        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} {'Status'.ljust(25)} | {'Cadastro'.ljust(25)}')
+    def alternar_estado(cls):
+        codigo = input("Digite o código do restaurante que deseja ativar/desativar: ")
+        restaurante_encontrado = False
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_notas).ljust(25)} | {restaurante.ativo.ljust(22)} | {str(restaurante._codigo_unico).ljust(25)}' )
+            if str(restaurante._codigo_unico) == codigo:
+                restaurante._ativo = not restaurante._ativo
+                print(f"O restaurante '{restaurante._nome}' teve seu estado alterado para {'Ativo' if restaurante._ativo else 'Inativo'}")
+                restaurante_encontrado = True
+                break
+        if not restaurante_encontrado:
+            print("Restaurante não encontrado.")
 
     @property
     def ativo(self):
-        return '✅'if self._ativo else '❌'
-    
-    def alternar_estado(self):
-        self._ativo = not self._ativo
-        
+        return '✅' if self._ativo else '❌'
+
     def receber_avaliacao(self, cliente, nota):
         if isinstance(nota, int) and 0 < nota <= 5:
             avaliacao = Avaliacao(cliente, nota)
             self._avaliacao.append(avaliacao)
-        
+
     @property
     def media_notas(self):
         if not self._avaliacao:
@@ -80,15 +74,14 @@ class Restaurante:
     def adicionar_no_cardapio(self, item):
         if isinstance(item, ItemCardapio):
             self._cardapio.append(item)
-            
+
     @property
     def exibir_cardapio(self):
         print(f'Cardápio do restaurante: {self._nome}')
-        for i, item in enumerate(self._cardapio,1):  
+        for i, item in enumerate(self._cardapio, 1):
             if hasattr(item, 'descricao'):
                 mensagem_prato = f'{i}. Nome:{item._nome} | Preço: R${item._preco} | Descrição: {item.descricao}'
                 print(mensagem_prato)
             else:
                 mensagem_bebida = f'{i}. Nome:{item._nome} | Preço: R${item._preco} | Tamanho: {item.tamanho}'
                 print(mensagem_bebida)
-    
